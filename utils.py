82dd22905655eb8eeaffaab8d3ea92becb0838cc
FiniteSingularity <finitesingularityttv@gmail.com>
2022-08-09 17:10:14 -0500

    Fixes issue with jpeg-images scrolling across the matrix.

--- utils.py
+++ utils.py
@@ -12,9 +12,14 @@ def process_data(data):
             frame = create_text_frame(row)
             text[index] = frame
             continue
-        img_id = row["value"]
-        if img_id not in images:
-            images[img_id] = create_image_frame(img_id)
+        elif row["mc_type"] == "emote":
+            img_id = row["value"]
+            if img_id not in images:
+                images[img_id] = create_emote_frame(img_id)
+        elif row["mc_type"] == "image":
+            url = row["value"]
+            if url not in images:
+                images[url] = create_image(url)
 
     return (images, text)
 
@@ -47,8 +52,7 @@ def create_text_frame(data_row, fill_color=(255, 255, 0, 255)):
     return frames
 
 
-def create_image_frame(img_id):
-    url = f"https://static-cdn.jtvnw.net/emoticons/v2/{img_id}/default/light/1.0"
+def create_image(url):
     img_data = urlopen(url).read()
     image = Image.open(io.BytesIO(img_data))
 
@@ -58,7 +62,7 @@ def create_image_frame(img_id):
         "current_frame": 0,
         "current_frame_start": 0,
     }
-    for frame_num in range(0, image.n_frames):
+    for frame_num in range(0, getattr(image, "n_frames", 1)):
         image.seek(frame_num)
         duration = image.info.get('duration', 0)
 
@@ -69,10 +73,20 @@ def create_image_frame(img_id):
     
     return image_dat
 
+def create_emote_frame(img_id):
+    url = f"https://static-cdn.jtvnw.net/emoticons/v2/{img_id}/default/light/1.0"
+    return create_image(url)
 
 def char_type(unicode_cat):
+    print(unicode_cat)
     if unicode_cat == 'So':
         return 'emoji'
+    elif unicode_cat == 'Cf':
+        return 'Cf'
+    elif unicode_cat == 'Sk':
+        return 'Sk'
+    elif unicode_cat == 'Mn':
+        return 'Mn'
     elif unicode_cat == 'Co':
         return 'special'
     else:
@@ -92,7 +106,15 @@ def segment(text, seg_range):
 
 
 def split_by_unicode_group(text):
-    char_cats = [char_type(unicodedata.category(char)) for char in text]
+    filtered_text = []
+    for char in text:
+        ct = char_type(unicodedata.category(char))
+        if ct != 'Mn' and ct != 'Sk' and ct != 'Cf':
+            filtered_text.append(char)
+    filtered_text = ''.join(filtered_text)
+
+    char_cats = [char_type(unicodedata.category(char)) for char in filtered_text]
+    char_cats = list(filter(lambda x: (x != 'Mn' and x != 'Sk' and x != 'Cf'), char_cats))
     segment_ranges = []
     last_cat = ''
     for i, cat in enumerate(char_cats):
@@ -102,5 +124,5 @@ def split_by_unicode_group(text):
                 segment_ranges[-1]['range'][1] = i
             segment_ranges.append({'range': [i,-1], 'cat': cat})
     
-    segments = [segment(text, seg_range) for seg_range in segment_ranges]
+    segments = [segment(filtered_text, seg_range) for seg_range in segment_ranges]
     return segments
